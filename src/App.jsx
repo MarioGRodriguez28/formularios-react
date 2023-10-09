0e17334a2d27fb4f3215ac1d1a6ea240b36b15fa
Mario Rodriguez Martinez <mariog.rodriguezm@gmail.com>
2023-10-09 13:02:08 +0200

    UseEffect y LocalStorage, done

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,52 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import Formulario from "./components/Formulario";
 import Todos from "./components/Todos";
 
-const initialStateTodos = [
-  {
-    id: 1,
-    title: "Todo #01",
-    description: "Descripción #01",
-    state: "pendiente",
-    priority: true,
-  },
-  {
-    id: 2,
-    title: "Todo #02",
-    description: "Descripción #02",
-    state: "completado",
-    priority: false,
-  },
-];
+const initialStateTodos = JSON.parse(localStorage.getItem('todos')) || [];
 
 const App = () => {
   const [todos, setTodos] = useState(initialStateTodos);
 
- const addTodo= todo => {
-  setTodos([...todos, todo])
- }
+  useEffect(()=>{
+    localStorage.setItem('todos', JSON.stringify(todos))
+  },[todos])
 
- const deleteTodo = id=> {
-  const newArray = todos.filter(todo=> todo.id !== id)
-  setTodos(newArray)
- }
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
 
- const updateTodo= (id) =>{
-  const newArray = todos.map ((todo)=>{
-    if (todo.id === id){
-      todo.state=!todo.state
-    }
-    return todo;
-  })
-  setTodos(newArray)
- }
+  const deleteTodo = (id) => {
+    const newArray = todos.filter((todo) => todo.id !== id);
+    setTodos(newArray);
+  };
+
+  const updateTodo = (id) => {
+    const newArray = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.state = !todo.state;
+      }
+      return todo;
+    });
+    setTodos(newArray);
+  };
+
+  const orderTodo = (arrayTodos) => {
+    return arrayTodos.sort((a, b) => {
+      if (a.priority === b.priority) return 0;
+      if (a.priority) return -1;
+      if (!a.priority) return 1;
+    });
+  };
   return (
     <div className="container mb-2">
       <h1 className="my-5">Formularios</h1>
-      <Formulario addTodo={addTodo}/>
-      <Todos todos={todos} deleteTodo = {deleteTodo} updateTodo={updateTodo} />
+      <Formulario addTodo={addTodo} />
+      <Todos 
+      todos={orderTodo (todos)} 
+      deleteTodo={deleteTodo} 
+      updateTodo={updateTodo} 
+
+      />
     </div>
   );
 };
